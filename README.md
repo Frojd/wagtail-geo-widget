21c5f7e982f9b2eef75ffb54371647cf46a7a6ad
Martin Sandström <martin@marteinn.se>
2016-11-04 07:49:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Wagtail-Geo-Widget
+
+
+
+
+## Requirements
+
+- Python 2.7 / Python 3.4 / PyPy
+- Django 1.8+
+
+
+## Installation
+
+Install the library with pip:
+
+```
+$ pip install wagtail-geo-widget
+```
+
+
+## Quick Setup
+
+Make sure wagtail_geo_widget is added to your `INSTALLED_APPS`.
+
+```python
+INSTALLED_APPS = (
+    # ...
+    'wagtail_geo_widget',
+)
+
+```
+
+
+This should be enough to get started.
+
+
+## Usage
+
+
+## Full example
+
+
+
+## Settings
+
+- `GOOGLE_MAPS_V3_APIKEY`: Api key for google maps
+
+
+## Tests
+
+This library include tests, just run `python runtests.py`.
+
+
+## Contributing
+
+Want to contribute? Awesome. Just send a pull request.
+
+
+## License
+
+Wagtail-Geo-Widget is released under the [MIT License](http://www.opensource.org/licenses/MIT).
+
---
+++ wagtailgeowidget/static/wagtailgeowidget/js/geo-field.js
@@ -0,0 +1,140 @@
+"use strict";
+
+function GeoField(options) {
+    var defaultLocation = {lat: 59.3293, lng: 18.0686};
+
+    if (options.defaultLocation) {
+        defaultLocation = options.defaultLocation;
+    }
+
+    defaultLocation = new google.maps.LatLng(
+        parseFloat(defaultLocation.lat),
+        parseFloat(defaultLocation.lng)
+    );
+
+    this.zoom = options.zoom;
+    this.srid = options.srid;
+    this.sourceField = $(options.sourceSelector);
+    this.addressField = $(options.addressSelector);
+    this.latLngField = $(options.latLngDisplaySelector);
+    this.geocoder = new google.maps.Geocoder();
+
+    this.initMap(options.mapEl, defaultLocation);
+    this.initEvents();
+
+    this.setMapPosition(defaultLocation);
+    this.updateLatLng(defaultLocation);
+}
+
+GeoField.prototype.initMap = function(mapEl, defaultLocation) {
+    var map = new google.maps.Map(mapEl, {
+        zoom: this.zoom,
+        center: defaultLocation
+    });
+
+    var marker = new google.maps.Marker({
+        position: defaultLocation,
+        map: map,
+        draggable: true
+    });
+
+    this.map = map;
+    this.marker = marker;
+}
+
+GeoField.prototype.initEvents = function() {
+    var self = this;
+
+    google.maps.event.addListener(this.marker, "dragend", function(event) {
+        self.setMapPosition(event.latLng);
+        self.updateLatLng(event.latLng);
+        self.writeLocation(event.latLng);
+    });
+
+    this.latLngField.on("input", function(e) {
+        var coords = $(this).val();
+        coords = coords.split(",").map(function(value) {
+            return parseFloat(value);
+        });
+
+        var latLng = new google.maps.LatLng(
+            coords[0],
+            coords[1]
+        );
+
+        self.setMapPosition(latLng);
+    });
+
+    this.addressField.on("input", function(e) {
+        clearTimeout(self._timeoutId);
+
+        var query = $(this).val();
+
+        self._timeoutId = setTimeout(function() {
+            self.geocodeSearch(query);
+        }, 400);
+    });
+}
+
+GeoField.prototype.geocodeSearch = function(query) {
+    var self = this;
+
+    this.geocoder.geocode({'address': query}, function(results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert('Google Maps Error: '+status);
+            return;
+        }
+
+        if (! results.length) {
+            alert('No results found');
+            return;
+        }
+
+        var latLng = results[0].geometry.location;
+
+        self.setMapPosition(latLng);
+        self.updateLatLng(latLng);
+        self.writeLocation(latLng);
+    });
+}
+
+GeoField.prototype.updateLatLng = function(latLng) {
+    this.latLngField.val(latLng.lat()+","+latLng.lng());
+}
+
+GeoField.prototype.setMapPosition = function(latLng) {
+    this.marker.setPosition(latLng);
+    this.map.setCenter(latLng);
+}
+
+GeoField.prototype.writeLocation = function(latLng) {
+    var lat = latLng.lat();
+    var lng = latLng.lng();
+    var value = 'SRID='+this.srid+';POINT('+lng+' '+lat+')';
+
+    this.sourceField.val(value);
+}
+
+var initializeGeoFields = function() {
+    $(".geo-field").each(function(index, el) {
+        var $el = $(el);
+
+        var data = window[$el.data('data-id')];
+        var options = {
+            mapEl: el,
+            sourceSelector: $(data.sourceSelector),
+            latLngDisplaySelector: $(data.latLngDisplaySelector),
+            zoom: data.zoom,
+            srid: data.srid,
+        }
+
+        options.addressSelector = data.addressSelector;
+        options.defaultLocation = data.defaultLocation;
+
+        new GeoField(options);
+    });
+}
+
+$(document).ready(function() {
+    google.maps.event.addDomListener(window, 'load', initializeGeoFields);
+});
