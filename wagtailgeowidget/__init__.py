4345207a0c094ef6bde21c6dedd7fdb821a0b343
Martin Sandström <martin@marteinn.se>
2016-11-07 07:32:03 +0100

    Added package details

---
+++ wagtailgeowidget/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+wagtailgeowidget
+----------
+Google Maps widget for the GeoDjango PointField field in Wagtail
+"""
+__title__ = "wagtailgeowidget"
+__version__ = "1.0.0"
+__build__ = 100
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2016 Fröjd Interactive"
