28bbd68b11214b57f51055c9a7b2a7490e054cbe
Martin Sandström <martin@marteinn.se>
2017-05-26 08:14:48 +0200

    Added test for negative lat/lng units

--- tests/test_latlng_parse.py
+++ tests/test_latlng_parse.py
@@ -9,3 +9,9 @@ class LatLngTestCase(TestCase):
         self.assertEquals(struct['srid'], '5432')
         self.assertEquals(struct['x'], '12.0')
         self.assertEquals(struct['y'], '13.0')
+
+    def test_negative_coords(self):
+        struct = geosgeometry_str_to_struct('SRID=5432;POINT(12.0 -13.0)')
+        self.assertEquals(struct['srid'], '5432')
+        self.assertEquals(struct['x'], '12.0')
+        self.assertEquals(struct['y'], '-13.0')
