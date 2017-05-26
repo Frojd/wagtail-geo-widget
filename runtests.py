4a5fb432a651939680b0a5f7e0a5d90613cebe83
Martin Sandström <martin@marteinn.se>
2017-05-26 08:02:31 +0200

    Created stub for test suite

---
+++ runtests.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+if not settings.configured:
+    params = dict(
+        LOGGING = {
+            'version': 1,
+            'disable_existing_loggers': False,
+            'handlers': {
+                'console': {
+                    'class': 'logging.StreamHandler',
+                },
+            },
+            'loggers': {
+                'wagtailgeowidget': {
+                    'handlers': ['console'],
+                    'level': 'ERROR',
+                    'propagate': True,
+                },
+            },
+        },
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+            }
+        },
+        INSTALLED_APPS=[
+            'django.contrib.contenttypes',
+            'django.contrib.auth',
+            'django.contrib.sites',
+            'wagtail.wagtailcore',
+            'wagtail.wagtailsites',
+            'wagtail.wagtailusers',
+            'wagtail.wagtailimages',
+            'taggit',
+            'wagtailgeowidget',
+            "tests",
+        ],
+        MIDDLEWARE_CLASSES=[],
+        ROOT_URLCONF='tests.urls',
+    )
+
+    settings.configure(**params)
+
+
+def runtests():
+    argv = sys.argv[:1] + ["test"] + sys.argv[1:]
+    execute_from_command_line(argv)
+
+
+if __name__ == "__main__":
+    runtests()
---
+++ tests/test_latlng_parse.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from wagtailgeowidget.helpers import geosgeometry_str_to_struct
+
+
+class LatLngTestCase(TestCase):
+    def test_invalid_coords(self):
+        struct = geosgeometry_str_to_struct('SRID=5432;POINT(12.0 13.0)')
+        self.assertEquals(struct['srid'], '5432')
+        self.assertEquals(struct['x'], '12.0')
+        self.assertEquals(struct['y'], '13.0')
