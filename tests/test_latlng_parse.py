ee02d9dd78495aa46e7dec8c3eb1f32b9fcf5352
Martin Sandström <martin@marteinn.se>
2017-05-26 08:14:08 +0200

    Renamed test unit to fit test functionality

--- tests/test_latlng_parse.py
+++ tests/test_latlng_parse.py
@@ -4,7 +4,7 @@ from wagtailgeowidget.helpers import geosgeometry_str_to_struct
 
 
 class LatLngTestCase(TestCase):
-    def test_invalid_coords(self):
+    def test_regular_coords(self):
         struct = geosgeometry_str_to_struct('SRID=5432;POINT(12.0 13.0)')
         self.assertEquals(struct['srid'], '5432')
         self.assertEquals(struct['x'], '12.0')
