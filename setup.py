f0df670fcd649ff7df42856eb64c00a9e147ffab
Thibaud Colas <thibaudcolas@gmail.com>
2017-11-06 15:36:40 +0200

    Add trove classifier to convey Python 3 support

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 2',
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         'Framework :: Django',
         'Topic :: Utilities',
     ],
