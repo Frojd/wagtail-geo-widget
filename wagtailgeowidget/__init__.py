0dc93684f36e2becfc367ffe01521aa2c44a48de
Martin Sandström <martin@marteinn.se>
2019-04-27 15:35:35 +0200

    Bump version

--- wagtailgeowidget/__init__.py
+++ wagtailgeowidget/__init__.py
@@ -4,12 +4,12 @@
 """
 wagtailgeowidget
 ----------
-A Google Maps widget for Wagtail that supports both GeoDjango PointField, StreamField and the standard CharField.
+Wagtail-Geo-Widget is the complete map solution for your Wagtail site.
 """
 
 __title__ = "wagtailgeowidget"
-__version__ = "5.0.1"
-__build__ = 501
+__version__ = "5.0.2"
+__build__ = 502
 __author__ = "Martin Sandström"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015-2019 Fröjd Interactive"
