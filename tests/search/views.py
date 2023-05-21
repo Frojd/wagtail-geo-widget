f1a864790813a30e2f80971609680dba33c72786
Martin Sandström <martin@marteinn.se>
2023-05-21 16:34:27 +0200

    Apply missing isort

--- tests/search/views.py
+++ tests/search/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
-from wagtail.search.models import Query
 from wagtail.models import Page
+from wagtail.search.models import Query
 
 
 def search(request):
