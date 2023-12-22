1a89b993faf7f0a59047f7e9ef4cb52e1192c434
Martin Sandström <martin@marteinn.se>
2023-12-22 06:46:06 +0100

    Improve typing

--- tests/test_latlng_parse.py
+++ tests/test_latlng_parse.py
@@ -1,3 +1,5 @@
+from typing import Dict, cast
+
 from django.test import SimpleTestCase
 
 from wagtailgeowidget.helpers import geosgeometry_str_to_struct
@@ -6,6 +8,7 @@ from wagtailgeowidget.helpers import geosgeometry_str_to_struct
 class LatLngParseTestCase(SimpleTestCase):
     def test_that_basic_parsing_works(self):
         struct = geosgeometry_str_to_struct("SRID=5432;POINT(12.0 13.0)")
+        struct = cast(Dict, struct)
 
         self.assertEqual(struct["srid"], "5432")
         self.assertEqual(struct["x"], "12.0")
@@ -18,6 +21,7 @@ class LatLngParseTestCase(SimpleTestCase):
 
     def test_that_optional_space_between_point_and_data_is_accepted(self):
         struct = geosgeometry_str_to_struct("SRID=5432;POINT (12.0 13.0)")
+        struct = cast(Dict, struct)
 
         self.assertEqual(struct["srid"], "5432")
         self.assertEqual(struct["x"], "12.0")
@@ -25,6 +29,8 @@ class LatLngParseTestCase(SimpleTestCase):
 
     def test_negative_coords(self):
         struct = geosgeometry_str_to_struct("SRID=5432;POINT(12.0 -13.0)")
+        struct = cast(Dict, struct)
+
         self.assertEqual(struct["srid"], "5432")
         self.assertEqual(struct["x"], "12.0")
         self.assertEqual(struct["y"], "-13.0")
