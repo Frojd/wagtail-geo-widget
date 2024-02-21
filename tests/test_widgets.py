807c4b75f27ef441aa22461bec92b260f923d64c
Katherine Domingo <katherine.domingo@torchbox.com>
2024-02-21 15:24:33 +0800

    Update tests from LeafletCase

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -45,20 +45,46 @@ class LeafletFieldTestCase(TestCase):
             },
         )
 
-        self.assertIn("new LeafletField", html)
+        if WAGTAIL_VERSION >= (6, 0):
+            self.assertIn('<input type="hidden" name="field" id="X" data-controller="leaflet-field"', html)
+        else:
+            self.assertIn("new LeafletField", html)
 
     def test_leaflet_field_js_init_contains_construct(self):
         widget = LeafletField()
-        html = widget.render_js_init("id", "field", "")
 
-        self.assertIn("new LeafletField", html)
+        if WAGTAIL_VERSION >= (6, 0):
+            html = widget.render(
+                "field",
+                "",
+                {
+                    "id": "X",
+                },
+            )
+            self.assertIn('<input type="hidden" name="field" id="X" data-controller="leaflet-field"', html)
+        else:
+            html = widget.render_js_init("id", "field", "")
+            self.assertIn("new LeafletField", html)
 
     def test_value_are_parsed_properly(self):
         widget = LeafletField()
-        html = widget.render_js_init("id", "field", "SRID=5432;POINT(12.0 13.0)")
 
-        self.assertIn('"lat": "13.0"', html)
-        self.assertIn('"lng": "12.0"', html)
+        if WAGTAIL_VERSION >= (6, 0):
+            from html import escape
+
+            html = widget.render(
+                "field",
+                "SRID=5432;POINT(12.0 13.0)",
+                {
+                    "id": "X",
+                },
+            )
+            self.assertIn(escape('"lat": "13.0"'), html)
+            self.assertIn(escape('"lng": "12.0"'), html)
+        else:
+            html = widget.render_js_init("id", "field", "SRID=5432;POINT(12.0 13.0)")
+            self.assertIn('"lat": "13.0"', html)
+            self.assertIn('"lng": "12.0"', html)
 
 
 class GeocoderFieldTestCase(TestCase):
